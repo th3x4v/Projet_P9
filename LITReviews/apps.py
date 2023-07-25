af907ed99b7d0af46bfbccec8091169296d4f7a3
Xavier Lonjon <xavierlonjon@yahoo.fr>
2023-07-25 16:54:32 +0200

    connection page of LITReviews app

---
+++ LITReviews/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class LitreviewsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'LITReviews'
---
+++ LITReviews/views.py
@@ -0,0 +1,42 @@
+
+from django.shortcuts import render
+from django.contrib.auth import login, logout, authenticate
+from django.shortcuts import redirect
+from accounts.models import User
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            error_message = 'Invalid username or password'
+    else:
+        error_message = ''
+    return render(request, 'LITReviews/login.html', {'error_message': error_message})
+
+def register_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        # Create a new user object
+        user = User.objects.create_user(username=username, password=password)
+        # Save the user object
+        user.save()
+
+        return redirect('login') 
+
+    return render(request, 'LITReviews/register.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
+
+def home_view(request):
+    return render(request, 'LITReviews/home.html')
+
